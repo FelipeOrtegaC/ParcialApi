55f4e665db8110198326ac989a7ff72cad3e4dc7
FelipeOrtegaC <139786453+FelipeOrtegaC@users.noreply.github.com>
2023-11-19 15:52:57 -0500

    Diseño de layouts implementado, cambio de linearlayout a gridlayout para ordenamiento de botones, ahora el boton se genera con textstyle bold y con el color del fondo

--- app/src/main/java/com/fruitsapi/MainActivity.kt
+++ app/src/main/java/com/fruitsapi/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.fruitsapi
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.GridLayout
 import android.widget.LinearLayout
 import android.widget.Spinner
 import androidx.compose.runtime.remember
@@ -25,8 +29,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-        val fruits = listOf(service.fruits)
-        val linearLayout = findViewById<LinearLayout>(R.id.mi_linear_layout)
+        val fruits = listOf("Manzana", "Banana", "Cereza", "Dátil", "Elderberry",
+            "Higo", "Grosella", "Higo chumbo", "Melón", "Jaca",
+            "Kiwi", "Limón", "Mango", "Nectarina", "Naranja",
+            "Papaya", "Piña", "Granada", "Frambuesa", "Fresa",
+            "Tamarindo", "Uva", "Sandía", "Xigua", "Yaca",
+            "Zarzamora", "Aguacate", "Bilimbi", "Carambola", "Durazno")
+        val linearLayout = findViewById<GridLayout>(R.id.mi_linear_layout)
 
         // Obtén una referencia al Spinner desde el XML
         val spinner: Spinner = findViewById(R.id.spinner)
@@ -42,8 +51,18 @@ class MainActivity : AppCompatActivity() {
 
         for (fruit in fruits) {
             val button = Button(this)
-            //button.text = fruit
+            button.text = fruit
             button.id = View.generateViewId()
+            button.setBackgroundColor(Color.parseColor("#EBEBE2"))
+            // Cambia el estilo del texto a negrita
+            button.setTypeface(null, Typeface.BOLD)
+            val layoutParams = GridLayout.LayoutParams()
+            // Convierte 5dp a píxeles
+            val marginsInDp = 5
+            val marginsInPx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, marginsInDp.toFloat(), resources.displayMetrics).toInt()
+            // Establece los márgenes del LayoutParams
+            layoutParams.setMargins(marginsInPx, marginsInPx, marginsInPx, marginsInPx)
+            button.layoutParams = layoutParams
             button.setOnClickListener {
                 val intent = Intent(this@MainActivity, NutrientesActivity::class.java)
                 startActivity(intent)
